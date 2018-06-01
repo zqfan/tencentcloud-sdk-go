346eab66c8aacb43ee6b7ad8f845d375d4ebcf30
ZhiQiang Fan <zhiqiangfan@tencent.com>
2018-06-01 00:57:18 -0700

    add request client for request & request id for error response

--- common/request.go
+++ common/request.go
@@ -154,6 +154,7 @@ func CompleteCommonParams(request Request, c *Client) {
 	params["Action"] = request.GetAction()
 	params["Timestamp"] = strconv.FormatInt(time.Now().Unix(), 10)
 	params["Nonce"] = strconv.Itoa(rand.Int())
+	params["RequestClient"] = "SDK_GO_0.1.1"
 }
 
 func ConstructParams(req Request) (err error) {
